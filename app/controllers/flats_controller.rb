93695dbada823d39f072b5e3a286174eca488bfb
Charlie Bertrand <charlie.bertrand@live.com>
2020-12-10 18:31:57 +0100

    adding all crud, starting front

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,50 @@
+class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(params_flat)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+     @flat = Flat.find(params[:id])
+    if @flat.update(params_flat)
+      redirect_to flat_path(@flat)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+private
+
+  def params_flat
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "---starting seeds----"
+
+10.times do |a|
+price = [10, 30, 50, 150].sample
+guest = [2, 3, 4, 5, 10].sample
+
+a = Flat.new(name: Faker::Space.star , address: Faker::Address.full_address, description: Faker::ChuckNorris.fact, price_per_night: price, number_of_guests: guest )
+puts "inserting #{a.name}"
+a.save!
+
+end
+puts "---seeds ending---"
